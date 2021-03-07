d5a3d1b44e0f8fa288ad49d7f1dfbcb5e1e2839e
takumi-pro <takumidiary.0927@gmail.com>
2021-03-07 22:39:35 +0900

    change app

--- src/app.js
+++ src/app.js
@@ -1,28 +1,4 @@
 import Vue from 'vue';
-// import SampleComponent from './components/sample-component.vue';
-
-// const app = new Vue({
-//     el: "#app",
-//     components: {
-//         SampleComponent
-//     }
-// })
-// var STORAGE_KEY = 'todos-vuejs-demo'
-// var todoStorage = {
-//   fetch: function() {
-//     var todos = JSON.parse(
-//       localStorage.getItem(STORAGE_KEY) || '[]'
-//     )
-//     todos.forEach(function(todo, index) {
-//       todo.id = index
-//     })
-//     todoStorage.uid = todos.length
-//     return todos
-//   },
-//   save: function(todos) {
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-//   }
-// }
 
 new Vue({
     el: "#app",
